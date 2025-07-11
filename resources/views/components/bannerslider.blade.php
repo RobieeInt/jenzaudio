01694c07f3c90250bd7f48ceb65e90b944928aa1
Robby Hernowo <robbyhernowo@gmail.com>
2025-07-11 09:59:55 +0700

    paket senang

--- resources/views/components/bannerslider.blade.php
+++ resources/views/components/bannerslider.blade.php
@@ -24,9 +24,15 @@
 <section class="position-relative pt-0 overflow-hidden mt-2">
     <div id="bannerCarousel" class="carousel slide carousel-fade mx-auto" style="max-width: 1160px;" data-bs-ride="carousel"
         data-bs-interval="2000">
+
+
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="{{ asset('redesign/images/newaudio.webp') }}" class="d-block w-100 banner-img" alt="Banner 1">
+            </div>
+            <div class="carousel-item active">
+                <img src="{{ asset('redesign/images/paketsenang.webp') }}" class="d-block w-100 banner-img"
+                    alt="Banner 1">
             </div>
             {{-- <div class="carousel-item">
                 <img src="{{ asset('redesign/images/bannerdetailing.png') }}" class="d-block w-100 banner-img"
